197602f7dee54ba88c761cf006b6ca7bef7a39ac
Egor <yegor.rabota.gu@mail.ru>
2020-11-17 14:14:02 +0300

    ezdazakop
    nice work. it was easy.

---
+++ ezdazakop/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/rent')
+def rent():
+    return render_template('rent.html')
+
+if __name__ == "__main__":
+    app.run()
